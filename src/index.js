ba2fd61ebd56f2e0deb1aefe15e7d21beb66a84a
NicoBarragan <nbarragan01@gmail.com>
2022-04-26 04:11:02 -0300

    feature: created first http server get response

---
+++ src/index.js
@@ -0,0 +1,9 @@
+app.get('/products', (req, res) => {
+    // Handle request
+    // res.send('This is for plain text)
+    // res.json({ type: "This is for JSON" })
+})
+
+app.post('/products', (req, res) => {
+    // Handle request
+})
